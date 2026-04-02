b732d8acab88e8c8846922f401e006d2e2fe1369
Akash-9794 <226301014@gkv.ac.in>
2026-04-02 11:13:34 +0530

    Refactored: Modular architecture with automated cloud training setup

--- app.py
+++ app.py
@@ -5,61 +5,6 @@ import subprocess
 import streamlit as st
 
 
-
-def run_app():
-    # rest all code
-    import joblib 
-    from src.utils import clean_text
-    
-    
-    # --- CACHED LOADING ---
-    @st.cache_resource
-    def load_assets():
-        return joblib.load("models/model.pkl"), joblib.load("models/vectorizer.pkl")
-    
-    model, vectorizer = load_assets()
-    
-    # --- SIDEBAR ---
-    with st.sidebar:
-        with st.container(horizontal_alignment="center"):
-           st.image("https://threedio-prod-var-cdn.icons8.com/wo/preview_sets/previews/QS7oEnf1jrYbjKU5.webp", width=100,)
-        st.title("About Project")
-        st.info("This AI uses Logistic Regression and TF-IDF to detect misinformation with 99% accuracy.")
-        st.markdown("---")
-        st.write("Developed by: Akash Chaurasiya")
-    
-    st.title("🔍 Fake News Detector")
-    st.markdown("# Combat misinformation with the power of Machine Learning.")
-    
-    st.subheader("Paste a news article or headline below")
-    
-    news_text = st.text_area("Enter or paste a news to verify fake or real",height = 200,placeholder="e.g., Reuters reports new budget fight in Congress...")
-    
-    # clean_text function 
-    
-    
-    if st.button("Verify Authenticity", use_container_width=True):
-        if news_text.strip() == "":
-            st.warning("⚠️ Please enter some text first!")
-        else:
-         with st.spinner("Analyzing linguistic patterns..."):
-    
-    
-           cleaned = clean_text(news_text) # for clean text
-           # transform using vectorizer
-           text_tfidf = vectorizer.transform([cleaned])
-           prediction = model.predict(text_tfidf)[0]
-           confidence = model.predict_proba(text_tfidf)
-           
-        #    show results 
-           if prediction == 1:
-                st.success(f"✅ REAL NEWS (Confidence: {confidence[0][1]*100:.2f}%)")  
-           else:
-                st.error(f"❌ FAKE NEWS (Confidence: {confidence[0][0]*100:.2f}%)")  
-    
-    
-    
-
 # Professional Check: If the model doesn't exist, run the setup script
 
 if not os.path.exists("models/model.pkl"):
@@ -68,4 +13,106 @@ if not os.path.exists("models/model.pkl"):
     st.success("Setup complete!")
 
 else:
-    run_app()
+       # rest all code
+
+   import joblib 
+
+   from src.utils import clean_text
+
+   
+
+   
+
+   # --- CACHED LOADING ---
+
+   @st.cache_resource
+
+   def load_assets():
+
+       return joblib.load("models/model.pkl"), joblib.load("models/vectorizer.pkl")
+
+   
+
+   model, vectorizer = load_assets()
+
+   
+
+   # --- SIDEBAR ---
+
+   with st.sidebar:
+
+       with st.container(horizontal_alignment="center"):
+
+          st.image("https://threedio-prod-var-cdn.icons8.com/wo/preview_sets/previews/QS7oEnf1jrYbjKU5.webp", width=100,)
+
+       st.title("About Project")
+
+       st.info("This AI uses Logistic Regression and TF-IDF to detect misinformation with 99% accuracy.")
+
+       st.markdown("---")
+
+       st.write("Developed by: Akash Chaurasiya")
+
+   
+
+   st.title("🔍 Fake News Detector")
+
+   st.markdown("# Combat misinformation with the power of Machine Learning.")
+
+   
+
+   st.subheader("Paste a news article or headline below")
+
+   
+
+   news_text = st.text_area("Enter or paste a news to verify fake or real , ask for  only news",height = 200,placeholder="e.g., Reuters reports new budget fight in Congress...")
+
+   
+
+   # clean_text function 
+
+   
+
+   
+
+   if st.button("Verify Authenticity", use_container_width=True):
+
+       if news_text.strip() == "":
+
+           st.warning("⚠️ Please enter some text first!")
+
+       else:
+
+        with st.spinner("Analyzing linguistic patterns..."):
+
+   
+
+   
+
+          cleaned = clean_text(news_text) # for clean text
+
+          # transform using vectorizer
+
+          text_tfidf = vectorizer.transform([cleaned])
+
+          prediction = model.predict(text_tfidf)[0]
+
+          confidence = model.predict_proba(text_tfidf)
+
+          
+
+       #    show results 
+
+          if prediction == 1:
+
+               st.success(f"✅ REAL NEWS (Confidence: {confidence[0][1]*100:.2f}%)")  
+
+          else:
+
+               st.error(f"❌ FAKE NEWS (Confidence: {confidence[0][0]*100:.2f}%)")  
+
+   
+
+   
+
+   
