a2d295407cde0cbfe715fa3dc99db4348e95bec0
Akash-9794 <226301014@gkv.ac.in>
2026-04-02 11:40:47 +0530

    Refactored: Modular architecture with automated cloud training setup

--- setup.sh
+++ setup.sh
@@ -10,8 +10,9 @@ mkdir -p data
 mkdir -p models
 
 # 2. Download the data from Hugging Face (Direct Download)
-curl -L https://huggingface.co/datasets/Akash-9794/fake-news-data/resolve/main/Fake.csv -o data/Fake.csv
-curl -L https://huggingface.co/datasets/Akash-9794/fake-news-data/resolve/main/True.csv -o data/True.csv
+curl -L "https://huggingface.co/datasets/Akai1014/fake-news-data/resolve/main/Fake.csv -o data/Fake.csv?download=true"
+curl -L "https://huggingface.co/datasets/Akai1014/fake-news-data/resolve/main/True.csv -o data/True.csv?download=true"
 
 # 3. Train the model on the server to create model.pkl and vectorizer.pkl
 python3 src/train.py
+
